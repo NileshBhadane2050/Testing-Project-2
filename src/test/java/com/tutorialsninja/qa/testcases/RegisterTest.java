6730c2e40402d4707eb8ef575a52d88bb4535560
NileshBhadane2050 <nileshbhadane765@gmail.com>
2024-02-23 01:04:30 +0530

    Update Comment in RegisterTest.Java

--- src/test/java/com/tutorialsninja/qa/testcases/RegisterTest.java
+++ src/test/java/com/tutorialsninja/qa/testcases/RegisterTest.java
@@ -15,6 +15,7 @@ public class RegisterTest extends Base {
 
 	// Variable
 	// Declaration of WebDriver variable[driver] to interact with the browser
+	//Updated Comment
 	public WebDriver driver;
 	RegisterPage registerPage; 
 	AccountSuccessPage accountSuccessPage;
